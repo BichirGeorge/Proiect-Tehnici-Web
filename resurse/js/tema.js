830d8b59c1e9530bf6a6993d594d47cabb55581f
Bichir George <bichirgeorgeionut@.gmail.com>
2024-05-16 21:11:25 +0300

    tema 6

--- resurse/js/tema.js
+++ resurse/js/tema.js
@@ -1,27 +1,38 @@
 window.addEventListener("DOMContentLoaded", function(){
     let tema = localStorage.getItem("tema");
     let body = document.body;
-    let temaIcon = document.getElementById("tema-icon");
+    let temaSwitch = document.getElementById("temaSwitch");
+    let temaIconMoon = document.querySelector(".bi-moon");
+    let temaIconSun = document.querySelector(".bi-sun");
+    let temaText = document.getElementById("temaText");
 
     if (tema === "dark") {
         body.classList.add("dark");
-        temaIcon.classList.add("fa-moon");
+        temaIconMoon.classList.remove("d-none");
+        temaIconSun.classList.add("d-none");
+        temaSwitch.checked = true;
+        temaText.textContent = "Dark Mode";
     } else {
         body.classList.remove("dark");
-        temaIcon.classList.add("fa-sun");
+        temaIconMoon.classList.add("d-none");
+        temaIconSun.classList.remove("d-none");
+        temaSwitch.checked = false;
+        temaText.textContent = "Light Mode";
     }
 
-    document.getElementById("schimba_tema").onclick = function(){
-        if(body.classList.contains("dark")){
-            body.classList.remove("dark");
-            temaIcon.classList.remove("fa-moon");
-            temaIcon.classList.add("fa-sun");
-            localStorage.removeItem("tema");
-        } else {
+    temaSwitch.addEventListener("change", function(){
+        if (temaSwitch.checked) {
             body.classList.add("dark");
-            temaIcon.classList.remove("fa-sun");
-            temaIcon.classList.add("fa-moon");
+            temaIconMoon.classList.remove("d-none");
+            temaIconSun.classList.add("d-none");
             localStorage.setItem("tema", "dark");
+            temaText.textContent = "Dark Mode";
+        } else {
+            body.classList.remove("dark");
+            temaIconMoon.classList.add("d-none");
+            temaIconSun.classList.remove("d-none");
+            localStorage.removeItem("tema");
+            temaText.textContent = "Light Mode";
         }
-    }
+    });
 });
